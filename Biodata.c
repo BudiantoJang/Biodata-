5aae1a376a9de729346373dd2daee4a2a1f7a547
Budianto Jang <budiantoj55@gmail.com>
2020-09-16 12:58:14 +0700

    biodata

---
+++ Biodata.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\t >>>  Biodata  <<< \n");
+	printf("Nama		: Budianto \n");
+	printf("Npm 		: 19081010057 \n");
+	printf("Jurusan		: Teknik Informatika\n");
+	printf("Angkatan	: 2019 \n");
+	return 0;
+}
